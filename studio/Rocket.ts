3baad44976444dc4d76cbad4c956e5e2f7ccc470
Micah <micahnyoung95@gmail.com>
2022-03-14 22:29:48 -0500

    finally found the tiny bugs

---
+++ studio/Rocket.ts
@@ -0,0 +1,50 @@
+import { Astronaut } from "./Astronaut";
+import { Cargo } from "./Cargo";
+import { Payload } from "./Payload";
+
+export class Rocket implements Payload {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+    massKg: number;
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg
+    }
+
+    sumMass(items: Payload[]):number {
+        let sum: number = 0;
+        for(let i = 0; i < items.length; i++) {
+            sum += items[i].massKg;
+        }
+    return sum;
+    }
+
+    currentMassKg(): number{
+        console.log("Hello")
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    }
+    canAdd(item: Payload): boolean{
+        if(this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+    }
+    addCargo(cargo: Cargo): boolean {
+        if(this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        } else if(!this.canAdd(cargo)) {
+            return false;
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if(this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        } else if(!this.canAdd(astronaut)) {
+            return false;
+        }
+    }
+}
